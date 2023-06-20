c9c4501df69cb692238b58998b69e08c0bf5d192
Crowdbotics <team@crowdbotics.com>
2023-06-20 07:52:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tanda-41613",
+  url: "https://tanda-41613.botics.co",
+  api: "https://tanda-41613.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
